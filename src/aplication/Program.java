ea70fa74aea35c849b90cc5d99d447dd5f671ec3
Flavio Yukio <flavio_ohphata@hotmail.com>
2026-05-26 18:36:44 -0300

    Primeiro commit

---
+++ src/aplication/Program.java
@@ -0,0 +1,56 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter name data ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data:" +product);
+		
+		System.out.println();
+		System.out.print("Enter the numer of product to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProduct(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data:" +product);
+		
+		System.out.println();
+		System.out.print("Enter the numer of product to be removed from in stock: ");
+	    quantity = sc.nextInt();
+		product.removeProduct(quantity);
+
+		System.out.println();
+		System.out.println("Updated data:" +product);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/entites/Product.java
@@ -0,0 +1,30 @@
+package entites;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProduct(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProduct(int quantity) {
+		this.quantity -= quantity;
+	}
+	public String toString() {
+		return name
+				+", $"
+				+ String.format("%.2f", price)
+				+","
+				+quantity
+				+" untils, Total: $"
+				+String.format("$%.2f" , totalValueInStock());
+	}
+	}
+
+
